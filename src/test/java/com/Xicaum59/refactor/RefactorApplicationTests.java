143426b0480848e42f02412dcf7ca6083c2c667b
Xico Utrabo <xico.utrabo@gmail.com>
2025-06-01 20:13:19 -0300

    Project created

---
+++ src/test/java/com/Xicaum59/refactor/RefactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Xicaum59.refactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
